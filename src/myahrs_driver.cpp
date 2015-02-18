4247c5c9777d4374af5a606b06606701ec32a821
robotpilot <passionvirus@gmail.com>
2015-02-18 21:38:21 +0900

    added test mode

--- src/myahrs_driver.cpp
+++ src/myahrs_driver.cpp
@@ -40,6 +40,8 @@ using namespace WithRobot;
 static const char* DIVIDER = "1";  // 100 Hz
 
 #define DEG2RAD (M_PI/180.0)
+#define RAD2DEG (180.0/M_PI)
+
 //------------------------------------------------------------------------------
 void handle_error(const char* error_msg)
 {
@@ -51,17 +53,14 @@ void handle_error(const char* error_msg)
 class MyAhrsDriverForROS : public iMyAhrsPlus
 {
 private:
-  ros::NodeHandle nh;
-  ros::NodeHandle nh_priv;
+  ros::NodeHandle nh_;
+  ros::NodeHandle nh_priv_;
 
   void OnSensorData(int sensor_id, SensorData data)
   {
-    sample_count++;
-    {
-      LockGuard _l(lock);
-      sensor_data = data;
-      sensor_data.euler_angle = sensor_data.quaternion.to_euler_angle();
-    }
+    LockGuard _l(lock);
+    sensor_data = data;
+    sensor_data.euler_angle = sensor_data.quaternion.to_euler_angle();
 
     publish_topic(sensor_id);
   }
@@ -72,37 +71,35 @@ private:
   }
 
 public:
-  int sample_count;
   ros::Publisher data_pub;
   sensor_msgs::Imu imu_data;
   Platform::Mutex lock;
   SensorData sensor_data;
   tf::TransformBroadcaster broadcaster;
 
-  std::string port;
-  int baud_rate;
-  std::string frame_id;
-  bool autocalibrate;
-  double linear_acceleration_stddev;
-  double angular_velocity_stddev;
-  double magnetic_field_stddev;
-  double orientation_stddev;
+  std::string port_;
+  int baud_rate_;
+  std::string frame_id_;
+  bool autocalibrate_;
+  double linear_acceleration_stddev_;
+  double angular_velocity_stddev_;
+  double magnetic_field_stddev_;
+  double orientation_stddev_;
 
   MyAhrsDriverForROS(std::string port="", unsigned int baudrate=115200)
   : iMyAhrsPlus(port, baudrate),
-    sample_count(0),
-    nh_priv("~")
+    nh_priv_("~")
   {
-    nh_priv.param("port", port, std::string("/dev/ttyACM0"));
-    nh_priv.param("baud", baud_rate, 115200);
-    nh_priv.param("frame_id", frame_id, std::string("imu_link"));
-    nh_priv.param("autocalibrate", autocalibrate, false);
-    nh_priv.param("linear_acceleration_stddev", linear_acceleration_stddev, 0.0);
-    nh_priv.param("angular_velocity_stddev", angular_velocity_stddev, 0.0);
-    nh_priv.param("magnetic_field_stddev", magnetic_field_stddev, 0.0);
-    nh_priv.param("orientation_stddev", orientation_stddev, 0.0);
+    nh_priv_.param("port", port_, std::string("/dev/ttyACM0"));
+    nh_priv_.param("baud", baud_rate_, 115200);
+    nh_priv_.param("frame_id", frame_id_, std::string("imu_link"));
+    nh_priv_.param("autocalibrate", autocalibrate_, false);
+    nh_priv_.param("linear_acceleration_stddev", linear_acceleration_stddev_, 0.0);
+    nh_priv_.param("angular_velocity_stddev", angular_velocity_stddev_, 0.0);
+    nh_priv_.param("magnetic_field_stddev", magnetic_field_stddev_, 0.0);
+    nh_priv_.param("orientation_stddev", orientation_stddev_, 0.0);
 
-    data_pub = nh_priv.advertise<sensor_msgs::Imu>("imu_publisher", 10);
+    data_pub = nh_.advertise<sensor_msgs::Imu>("imu/data", 10);
   }
 
   ~MyAhrsDriverForROS()
@@ -138,15 +135,13 @@ public:
 
   void publish_topic(int sensor_id)
   {
-    std::string line(50, '-');
-    printf("%s\n", line.c_str());
+    printf(".");
 
     Quaternion& q = sensor_data.quaternion;
     EulerAngle& e = sensor_data.euler_angle;
     ImuData<float>& imu = sensor_data.imu;
 
-    printf("%04d) sensor_id %d, Quaternion(xyzw)=%.4f,%.4f,%.4f,%.4f, Angle(rpy)=%.1f, %.1f, %.1f, Accel(xyz)=%.4f,%.4f,%.4f, Gyro(xyz)=%.4f,%.4f,%.4f, Magnet(xyz)=%.2f,%.2f,%.2f\n",
-      sample_count,
+    printf("sensor_id %d, Quaternion(xyzw)=%.4f,%.4f,%.4f,%.4f, Angle(rpy)=%.1f, %.1f, %.1f, Accel(xyz)=%.4f,%.4f,%.4f, Gyro(xyz)=%.4f,%.4f,%.4f, Magnet(xyz)=%.2f,%.2f,%.2f\n",
       sensor_id,
       q.x, q.y, q.z, q.w,
       e.roll, e.pitch, e.yaw,
@@ -175,7 +170,7 @@ public:
 
     data_pub.publish(imu_msg);
 
-    broadcaster.sendTransform(tf::StampedTransform(tf::Transform(tf::createQuaternionFromRPY(e.roll, -e.pitch, -e.yaw),
+    broadcaster.sendTransform(tf::StampedTransform(tf::Transform(tf::createQuaternionFromRPY(e.roll*DEG2RAD, -e.pitch*DEG2RAD, -e.yaw*DEG2RAD),
                                                                  tf::Vector3(0.0, 0.0, 0.1)),
                                                    ros::Time::now(), "imu_base", "imu"));
   }
@@ -187,19 +182,21 @@ int main(int argc, char* argv[])
 {
   ros::init(argc, argv, "myahrs_driver");
 
-  MyAhrsDriverForROS sensor(sensor.port, sensor.baud_rate);
+  MyAhrsDriverForROS sensor("/dev/ttyACM0", 115200);
 
   if(sensor.initialize() == false)
   {
     handle_error("initialize() returns false");
   }
 
-  while(1)
+  ros::Rate loop_rate(100); // 0.01sec
+
+  while (ros::ok())
   {
-    Platform::msleep(100);
+    ros::spinOnce();
+    loop_rate.sleep();
   }
 
-//  ros::spin();
   return 0;
 }
 
